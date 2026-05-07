48514ceff56c08bbf022d27318a79b897d341e53
Quien <quien697@gmail.com>
2026-05-07 16:17:39 +0800

    feat: add a new portfolio Medal Wall

--- src/components/Sections/Portfolio/portfolio.tsx
+++ src/components/Sections/Portfolio/portfolio.tsx
@@ -26,7 +26,7 @@ export default function PortfolioSection() {
                     <span className="text-4xl font-bold">{item.name}</span>
                     <span className="ml-4 text-neutral-400 text-sm">{item.status}</span>
                   </h1>
-                  <p className="text-xl leading-7">{item.description}</p>
+                  <p className="text-xl leading-7 whitespace-pre-line">{item.description}</p>
                   <ul>
                     <DetailList title={constants.technologies} isLink={false}>
                       {item.technologies.map((item, idx) => {
